17199b6f65a462c3901096d89ccabf2c9b31f500
desparzas7902 <desparzas7902@gmail.com>
2024-03-26 12:49:30 -0500

    edit main

--- main.py
+++ main.py
@@ -9,9 +9,22 @@ from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
 import json
 from selenium.common.exceptions import StaleElementReferenceException
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
 
 BASE_URL = "https://sel.migraciones.gob.pe/servmig-valreg/VerificarCE"
 IMG_SAVE_PATH = './src/temp/screenshot.png'
+
+@app.route('/api/get_carnet_extranjeria_data', methods=['POST'])
+def get_carnet_extranjeria_data():
+    data = request.get_json()
+    numero_carnet = data['numero_carnet']
+    dia = data['dia']
+    mes = data['mes']
+    anio = data['anio']
+    res_scrap = scrape_data(numero_carnet, dia, mes, anio)
+    return jsonify(res_scrap)
 
 def conectar(driver):
     wait_page = WebDriverWait(driver, 10)
@@ -50,7 +63,6 @@ def send_form(driver):
         btn_verificar = driver.find_element(By.ID, "ctl00_bodypage_btnverificar")
         btn_verificar.click()
         print("Formulario enviado")
-        # esperar a que cargue la página
         wait_page.until(EC.staleness_of(btn_verificar))
     except Exception as e:
         print("Error al enviar el formulario:", e)
@@ -111,6 +123,20 @@ def verificar_llenado(driver):
         print("Elemento no encontrado")
         return False
 
+def invalid_form_response(driver):
+    try:
+        data_element = driver.find_element(By.ID, "ctl00_bodypage_pnlAlerta")
+
+        texto_alerta = data_element.get_attribute('innerText')
+        texto_alerta = texto_alerta.strip()
+        if texto_alerta != "" and "El código de verificación no es correcto." not in texto_alerta:
+            print("MENSAJE DE ALERTA: '", texto_alerta, "'")
+            return True, texto_alerta
+        return False, ""
+    except Exception as e:
+        print("NO SE MOSTRÓ PANEL DE ALERTA")
+        return False, ""
+
 def valid_form_response(driver):
     try:
         data_element = driver.find_element(By.ID, "ctl00_bodypage_pnlData")
@@ -128,7 +154,7 @@ def get_captcha_text(img_filepath):
     captcha_text = captcha_text.upper()
     return captcha_text
 
-def save_data(driver):
+def get_data(driver):
     wait_elements = WebDriverWait(driver, 10)
     try:
         nombre_completo = wait_elements.until(EC.presence_of_element_located((By.ID, "ctl00_bodypage_lblnombre")))
@@ -138,29 +164,104 @@ def save_data(driver):
         vencimiento_residencia = wait_elements.until(EC.presence_of_element_located((By.ID, "ctl00_bodypage_lblfecha_residencia")))
         caducidad_carnet = wait_elements.until(EC.presence_of_element_located((By.ID, "ctl00_bodypage_lblmensaje_cad")))
         emision_carnet = wait_elements.until(EC.presence_of_element_located((By.ID, "ctl00_bodypage_lblmensaje_emi")))
+        nombre_completo = nombre_completo.get_attribute('innerText')
+        nacionalidad = nacionalidad.get_attribute('innerText')
+        fecha_nacimiento = fecha_nacimiento.get_attribute('innerText')
+        calidad_migratoria = calidad_migratoria.get_attribute('innerText')
+        vencimiento_residencia = vencimiento_residencia.get_attribute('innerText')
+        caducidad_carnet = caducidad_carnet.get_attribute('innerText')
+        emision_carnet = emision_carnet.get_attribute('innerText')
+#         {
+# 	"caducidad_carnet": "21/02/2019",
+# 	"calidad_migratoria": "TRABAJADOR",
+# 	"emision_carnet": "21/02/2014",
+# 	"fecha_nacimiento": "24-12-1977",
+# 	"nacionalidad": "ESPAÑOLA",
+# 	"nombre_completo": "MARTINEZ CAMPO, PLACIDO",
+# 	"vencimiento_residencia": "18/02/2016"
+# }
+        # format the data
+        # dates will use - not /
+        caducidad_carnet = caducidad_carnet.replace("/", "-")
+        emision_carnet = emision_carnet.replace("/", "-")
+        fecha_nacimiento = fecha_nacimiento.replace("/", "-")
+        vencimiento_residencia = vencimiento_residencia.replace("/", "-")
+        nombre_completo = nombre_completo.replace(",", "")
+        nombre_array = nombre_completo.split(" ")
+        apellido1 = nombre_array[0]
+        apellido2 = nombre_array[1]
+        nombre1 = nombre_array[2]
+        nombre2 = ""
+        nombre3 = ""
+        # si hay mas de 3 nombres
+        if len(nombre_array) > 3:
+            nombre2 = nombre_array[3]
+        if len(nombre_array) > 4:
+            nombre2 = nombre_array[3]
+            nombre3 = nombre_array[4]
         data = {
-                "nombre_completo": nombre_completo.get_attribute('innerHTML'),
-                "nacionalidad": nacionalidad.get_attribute('innerHTML'),
-                "fecha_nacimiento": fecha_nacimiento.get_attribute('innerHTML'),
-                "calidad_migratoria": calidad_migratoria.get_attribute('innerHTML'),
-                "vencimiento_residencia": vencimiento_residencia.get_attribute('innerHTML'),
-                "caducidad_carnet": caducidad_carnet.get_attribute('innerHTML'),
-                "emision_carnet": emision_carnet.get_attribute('innerHTML')
+                "nacionalidad": nacionalidad,
+                "fecha_nacimiento": fecha_nacimiento,
+                "calidad_migratoria": calidad_migratoria,
+                "vencimiento_residencia": vencimiento_residencia,
+                "caducidad_carnet": caducidad_carnet,
+                "emision_carnet": emision_carnet,
+                "apellido1": apellido1,
+                "apellido2": apellido2,
+                "nombre1": nombre1,
+                "nombre2": nombre2,
+                "nombre3": nombre3
         }
         print('-----------------------------------------')
         print("Data RESPONSE: ", data)
         print('-----------------------------------------')
-        with open('./src/data/data.json', 'w', encoding='utf-8') as file:
-            json.dump(data, file, ensure_ascii=False, indent=4)
+        return data
     except NoSuchElementException:
         print("No se encontraron elementos en la página. Saliendo...")
     except TimeoutException:
         print("Tiempo de espera excedido. Saliendo...")
     except StaleElementReferenceException:
         print("Algunos elementos ya no están en el DOM. Saliendo...")
-    return True
+
+def validate_input(numero_carnet, dia, mes, anio):
+    if numero_carnet == "" or dia == "" or mes == "" or anio == "":
+        return False, "Los campos no deben estar vacíos"
+    if len(numero_carnet) != 9:
+        return False, "El número de carnet debe tener 9 dígitos"
+    try:
+        dia = int(dia)
+        mes = int(mes)
+        anio = int(anio)
+        if dia < 1 or dia > 31:
+            return False, "El día debe estar entre 1 y 31"
+        if mes < 1 or mes > 12:
+            return False, "El mes debe estar entre 1 y 12"
+        if anio < 1900 or anio > 2024:
+            return False, "El año debe estar entre 1900 y 2024"
+    except ValueError:
+        return False, "Los campos de fecha deben ser numéricos"
+    try:
+        int(numero_carnet)
+    except ValueError:
+        return False, "El número de carnet debe ser numérico"
+
+    return True, ""
 
 def scrape_data (numero_carnet, dia, mes, anio):
+
+    valid_input, error_message = validate_input(numero_carnet, dia, mes, anio)
+
+    if not valid_input:
+        json_response = {
+            "error": error_message
+        }
+        print('--------------------------------')
+        print(json_response)
+        print ('--------------------------------')
+        return json_response
+
+    data_res = {}
+
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
     # chrome_options.add_argument('--no-sandbox')
@@ -174,7 +275,6 @@ def scrape_data (numero_carnet, dia, mes, anio):
     chrome_options.add_argument('--disable-popup-blocking')
     chrome_options.add_argument('--disable-translate')
     chrome_options.add_argument('--ignore-certificate-errors')
-    # options.add_argument('--headless')
     driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(10)
     driver.maximize_window()
@@ -194,15 +294,38 @@ def scrape_data (numero_carnet, dia, mes, anio):
             if verificar_llenado(driver):
                 print("Formulario llenado correctamente")
                 send_form(driver)
+                datos_enviados_invalidos, mensaje_alerta = invalid_form_response(driver)
+                if (datos_enviados_invalidos):
+                    print("Datos enviados inválidos")
+                    break
                 if (valid_form_response(driver)):
                     vfr = True
-                    save_data(driver)
+                    data_res = get_data(driver)
             else:
                 print("Error al llenar el formulario")
             print('--------------------------------')
             print('--------------------------------')
         except Exception as e:
             print("Error al conectar a la página:", e)
+            driver.quit()
+
+    driver.quit()
+
+    if datos_enviados_invalidos:
+        # CREAR UN JSON
+        json_response = {
+            "error": mensaje_alerta
+        }
+        print('--------------------------------')
+        print(json_response)
+        print('--------------------------------')
+        return json_response
+    else:
+        print('--------------------------------')
+        print(data_res)
+        print('--------------------------------')
+        return data_res
+        
 
 def main():
     numero_carnet = "001043328"
@@ -212,4 +335,5 @@ def main():
     scrape_data(numero_carnet, dia, mes, anio)
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
+    # main()
